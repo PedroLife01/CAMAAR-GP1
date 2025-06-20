ee6a95a48b86618031059ec3a0df30db8a646ca9
Life <82733768+PedroLife01@users.noreply.github.com>
2025-06-20 19:26:36 -0300

    feat: BDD visualizar template

---
+++ features/step_definitions/visualizar_templates_steps.rb
@@ -0,0 +1,25 @@
+# features/step_definitions/visualizar_templates_steps.rb
+
+Given('estou autenticado como {string}') do |_papel|
+  pending 'login admin'
+end
+
+Given('possuo {int} templates cadastrados') do |_quantidade|
+  pending 'criar N templates'
+end
+
+Given('não possuo templates cadastrados') do
+  pending 'garantir lista vazia'
+end
+
+When('visito a página {string}') do |_pagina|
+  pending 'visitar página templates'
+end
+
+Then('devo ver {int} linhas na tabela de templates') do |_quantidade|
+  pending 'contar linhas da tabela'
+end
+
+Then('devo ver a mensagem {string}') do |_mensagem|
+  pending 'mensagem sem templates'
+end
