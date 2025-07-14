12784449d7f44babbea8d96e2630b558447318a5
Lucas Fernandes da Silveira Campos <108770564+l-campos@users.noreply.github.com>
2025-07-14 00:04:25 -0300

    Update test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+  
 
 
   # Raise error when a before_action's only/except options reference missing actions.
